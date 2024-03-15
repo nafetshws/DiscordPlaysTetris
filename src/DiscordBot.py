3fadb574d12e094870dae19b38e0bd8caedae5ee
nafetshws <stefan0507hws21@gmail.com>
2024-03-15 12:43:34 +0100

    add abillity to count reactions

--- src/DiscordBot.py
+++ src/DiscordBot.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 from TetrisBot import TetrisBot
 import json
 import os
+import time
+import asyncio
+import emoji 
 
 def load_token():
     try:
@@ -14,14 +17,26 @@ def load_token():
 class TetrisBotForDiscord(commands.Bot):
     def __init__(self, command_prefix, intents):
         super().__init__(command_prefix=command_prefix, intents=intents)
-        self.default_text_channel = None
+        self.default_text_channel : discord.TextChannel = None
         self.command_prefix = command_prefix
         self.tetris_bot = None
         self.game_instance_running = False
+
+        #emojis
+        self.down = ":down_arrow:"
+        self.left = ":left_arrow:"
+        self.right = ":right_arrow:"
+        self.rotate = ":arrows_clockwise:"
+
         self.add_commands()
  
     async def on_ready(self):
         print("Hello {}".format(self.user.name))
+
+    async def on_reaction_add(self, reaction : discord.Reaction, user : discord.User):
+        pass
+        #print(user.name)
+        #print(reaction.emoji)
 
     async def write(self, ctx, msg):
         if self.default_text_channel is None:
@@ -32,33 +47,57 @@ class TetrisBotForDiscord(commands.Bot):
     async def send_current_board(self):
         with open(os.path.dirname(__file__) + "/../res/current_board.png", "rb") as f:
             image = discord.File(f)
-            await self.default_text_channel.send(file=image)
+            return await self.default_text_channel.send(file=image)
+
+    #returns a dict with the amount of certain emojis
+    async def count_reactions(self, ctx, msg_id):
+        reaction_count = {f"{self.left}": 0,
+                          f"{self.right}": 0,
+                          f"{self.down}": 0,
+                          f"{self.rotate}": 0}
+        #fetch message from discord (not from cache, because reactions aren't added yet)
+        msg : discord.Message = await ctx.fetch_message(msg_id) 
+
+        for reaction in msg.reactions:
+            #get string representation of emoji 
+            e = emoji.demojize(reaction.emoji)
+            #the emoji module and discord use different names for the emoji. Conversion is necessary.
+            if "clockwise" in e:
+                e = self.rotate
+
+            #increment emoji count in dict
+            if e in reaction_count:
+                reaction_count[e] += 1
+
+        return reaction_count
 
     def add_commands(self):
         @self.command(name="default")
         async def set_default_text_channel(ctx, channel : discord.TextChannel):
             self.default_text_channel = channel
 
-            up = ":arrow_up:"
-            down = ":arrow_down:"
-            left = ":arrow_left:"
-            right = ":arrow_right:"
-            rotate = ":arrows_clockwise:"
 
-            DEFAULT_CHANNEL_MSG = "This is now the default text channel of <@{}>\nHow to engange with the bot react with the following Emojis or use the following commands:\n\n**Start a new game**: {}start\n**Move left**: {}\n**Move right**: {}\n**Drop down**: {}\n**Rotate (clockwise)**: {}".format(self.user.id, self.command_prefix, left, right, down, rotate)
+            DEFAULT_CHANNEL_MSG = "This is now the default text channel of <@{}>\nHow to engange with the bot react with the following Emojis or use the following commands:\n\n**Start a new game**: {}start\n**Move left**: {}\n**Move right**: {}\n**Drop down**: {}\n**Rotate (clockwise)**: {}".format(self.user.id, self.command_prefix, self.left, self.right, self.down, self.rotate)
             message = await self.default_text_channel.send(DEFAULT_CHANNEL_MSG)
             await message.pin()
 
         @self.command("start")
         async def start_game(ctx):
-            if not self.game_instance_running:
-                self.write("There is already a running instance of the game!")
+            if self.game_instance_running:
+                await self.write(ctx, "There is already a running instance of the game!")
                 return                                
 
             self.game_instance_running = True
             self.tetris_bot = TetrisBot()
             self.tetris_bot.start_game()
-            await self.send_current_board()
+            msg = await self.send_current_board()
+
+            #wait 30s
+            await asyncio.sleep(10)
+
+            await self.count_reactions(ctx, msg.id)
+
+
 
 if __name__ == "__main__":
     intents = discord.Intents.all()
